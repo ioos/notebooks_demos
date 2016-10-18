267f76f6a1c89f0d6a7a3f0503b67d824447c41b
ocefpaf <ocefpaf@gmail.com>
2016-10-18 10:28:21 -0300

    Auto deploy gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
+
+# Exit with nonzero exit code if anything fails.
+set -e
+
+# Save some useful information
+REPO=$(git config remote.origin.url)
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+# Get the commit sha since gh-pages has no history.
+SHA=$(git rev-parse --verify HEAD)
 
 # Go to the out directory and create a *new* Git repo.
 cd $GH_PAGES_DIR
 git init
 
-# Inside this git repo we'll pretend to be a new user.
-git config user.name "Travis CI"
-git config user.email "travis@nobody.org"
+# Config the dummy git user.
+git config user.name "Travis-CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-echo Pushing...
-git push --quiet --force https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages 2>&1 >/dev/null
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
+# Now that we're all set up, we can push.
+git push --quiet --force $SSH_REPO gh-pages
