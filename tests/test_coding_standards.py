927744be09291b4e3e6351ac5fc85b4f42214a88
Filipe Fernandes <ocefpaf@gmail.com>
2019-02-15 12:40:44 -0200

    modernize the tests

--- tests/test_coding_standards.py
+++ tests/test_coding_standards.py
@@ -18,15 +18,15 @@ from utilities import check_coding_standard
 
 _root_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
-nblist = glob(os.path.join(_root_path, 'notebooks', '*.ipynb'))
+nblist = glob(os.path.join(_root_path, "notebooks", "*.ipynb"))
 
 passed = True
 for ipynb in sorted(nblist):
     try:
         check_coding_standard(ipynb)
-        print('[PASSED]: {}'.format(os.path.split(ipynb)[-1]))
+        print("[PASSED]: {}".format(os.path.split(ipynb)[-1]))
     except Exception as e:
-        print('[FAILED]: {}'.format(os.path.split(ipynb)[-1]))
+        print("[FAILED]: {}".format(os.path.split(ipynb)[-1]))
         print(e)
         passed = False
 
